aa4b368e34fe2a342b6c6934d943890920e08f28
Christos Korompokis <christos.korompokis@oddschecker.com>
2022-03-01 17:43:41 +0200

    add some storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,7 +11,8 @@ module.exports = {
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-actions',
-    '@storybook/addon-controls'
+    '@storybook/addon-controls',
+    '@storybook/addon-viewport'
   ],
   framework: '@storybook/react',
 };

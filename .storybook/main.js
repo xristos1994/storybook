3f1930236894590acd48da10c82211c46d5d8235
Christos Korompokis <christos.korompokis@oddschecker.com>
2022-04-22 16:17:03 +0300

    Fix storybook main addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,7 @@ module.exports = {
     '@storybook/addon-storyshots-puppeteer',
     '@storybook/addon-storyshots',
     '@storybook/addon-a11y',
-    '@storybook/source-loader',
+    // '@storybook/source-loader',
     '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-controls',
